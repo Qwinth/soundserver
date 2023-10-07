38277f17577aed6657ef9f66bf8cd615dd286a52
Qwinth <76917271+Qwinth@users.noreply.github.com>
2023-10-07 12:15:01 +0300

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -3,45 +3,28 @@
 #include <thread>
 #include <map>
 #include <vector>
+#include <chrono>
 #include <csignal>
 #include "alsaLib.hpp"
 #include "cpplibs/ssocket.hpp"
 #include "cpplibs/libjson.hpp"
+#include "cpplibs/argparse.hpp"
 using namespace std;
-
-enum Mode {
-    PLAY,
-    CAPTURE
-};
 
 struct cl_data {
     Mode mode;
-    int rate;
-    int channels;
-    int bitdepth;
+    WAVHeader header;
 };
 
-_snd_pcm_format inttoformat(int i) {
-    switch (i)
-    {
-    case 16:
-        return SND_PCM_FORMAT_S16_LE;
-        break;
 
-    // case 24:
-    //     return SND_PCM_FORMAT_S24_LE;
-    //     break;
-    
-    default:
-        return SND_PCM_FORMAT_S32_LE;
-        break;
-    }
-}
+
+long double doubleTime() { return std::chrono::duration_cast<std::chrono::duration<long double>>(std::chrono::system_clock::now().time_since_epoch()).count(); }
 
 // vector<cl_data> play_buff;
 map<string, string> play_status;
 map<string, cl_data> play_setup;
 map<string, PCM*> pcms;
+string globalDevice;
 
 // void soundwriter() {
 //     PCM pcm("default", SND_PCM_STREAM_PLAYBACK, 0);
@@ -102,24 +85,18 @@ void player(SSocket client) {
     while (play_setup.find(id) == play_setup.end()) continue;
 
     Mode mode = play_setup[id].mode;
-    int rate = play_setup[id].rate;
-    int format = play_setup[id].bitdepth;
-    int channels = play_setup[id].channels;
-
     cout << "Player: connected user: " << id << endl;
 
-    PCM pcm("default", (_snd_pcm_stream)mode, 0);
-    pcm.setAccess(SND_PCM_ACCESS_RW_INTERLEAVED);
-    pcm.setFormat(inttoformat(format));
-    pcm.setChannels(channels);
-    pcm.setRate(rate);
+    PCM pcm;
+    pcm.setup(globalDevice, play_setup[id].header, mode);
+    try { pcm.setBufferSize(65536); } catch (int e) {};
     pcm.paramsApply();
     pcm.prepare();
 
     pcms[id] = &pcm;
 
     int period = pcm.getPeriod();
-    size_t buff_size = period * channels * (format / 8);
+    size_t buff_size = period * play_setup[id].header.numChannels * (play_setup[id].header.bitsPerSample / 8);
     client.ssend(to_string(buff_size));
 
     if (mode == PLAY) {
@@ -138,7 +115,7 @@ void player(SSocket client) {
                 
             // }
 
-            if (play_status[id] == "STOP") {
+            if (play_status[id] == "STOPPED") {
                 pcm.drop();
                 pcm.close();
                 client.sclose();
@@ -152,7 +129,7 @@ void player(SSocket client) {
                 while (play_status[id] == "PAUSED") { this_thread::sleep_for(1ms); }
             }
 
-            if (pcm.writei(wavdata.value, period) == -EPIPE) {// * (format / 8)
+            if (pcm.writei(wavdata.value, period) == -EPIPE) {
                 cout << "xrun" << endl;
                 pcm.recover(-EPIPE, 1);
             }
@@ -162,7 +139,7 @@ void player(SSocket client) {
         char buff[buff_size];
 
         while (true) {
-            if (play_status[id] == "STOP") {
+            if (play_status[id] == "STOPPED") {
                 pcm.drop();
                 pcm.close();
                 client.sclose();
@@ -176,7 +153,7 @@ void player(SSocket client) {
                 while (play_status[id] == "PAUSED") { this_thread::sleep_for(1ms); }
             }
 
-            if (pcm.readi(buff, period) == -EPIPE) {// * (format / 8)
+            if (pcm.readi(buff, period) == -EPIPE) {
                 cout << "xrun" << endl;
                 pcm.recover(-EPIPE, 1);
             }
@@ -189,40 +166,44 @@ void player(SSocket client) {
 
 void manager(SSocket sock) {
     Json json;
-
     string id = uuid4();
     play_status[id] = "NONE";
-    sock.ssend(id);
-
-    string mdata = sock.srecv(1024);
-    JsonNode node = json.parse(mdata);
-
-    if (node.size() < 4) {
-        sock.sclose();
-        return;
-    }
 
     cl_data cldata;
-    cldata.mode = (Mode)node["mode"].integer;
-    cldata.rate = node["rate"].integer;
-    cldata.bitdepth = node["width"].integer;
-    cldata.channels = node["channels"].integer;
+    cldata.header = *((WAVHeader*)sock.srecv_char(44).value);
+    cldata.mode = (Mode)stoi(sock.srecv(1));
+
+    JsonNode params = json.parse(sock.srecv(1024));
 
     play_setup[id] = cldata;
-    sock.ssend("ok");
+    sock.ssend(id);
+
+    long double start = 0;
+    long double duration = (long double)(cldata.header.subchunk2Size / (cldata.header.numChannels * (cldata.header.bitsPerSample / 8))) / (long double)cldata.header.sampleRate;
 
     cout << "Manager: connected user: " << id << endl;
 
     while (true) {
         string data = sock.srecv(1024);
-
+    
         if (data == "start") {
-            play_status[id] = "PREPARED";
+            play_status[id] = "RUNNING";
+            start = doubleTime();
         } else if (data == "pause") {
             play_status[id] = "PAUSED";
         } else if (data == "stop" || data.length() == 0) {
-            play_status[id] = "STOP";
+            // cout << pcms[id]->bufferAvailable() << endl;
+            play_status[id] = "STOPPED";
             break;
+        } else if (data == "is_running") {
+            int preverrno = errno;
+
+            if (params["check_is_running"].str == "use_buffer_experimental") {
+                int frames = pcms[id]->bufferAvailable();
+                sock.ssend((frames >= pcms[id]->getBufferSize() - 100 || frames < 0) ? "0" : "1");
+            } else if (params["check_is_running"].str == "use_timer") sock.ssend((doubleTime() - start >= duration) ? "0" : "1");
+
+            errno = preverrno;
         }
     }
     sock.sclose();
@@ -317,7 +298,13 @@ void listener() {
 }
 
 
-int main() {
+int main(int argc, char** argv) {
+    ArgumentParser parser(argc, argv);
+    parser.add_argument({.flag1 = "-D", .flag2 = "--default-device"});
+    auto args = parser.parse();
+
+    globalDevice = (args["--default-device"].str != "false") ? args["--default-device"].str : "default";
+
     SSocket sock(AF_INET, SOCK_STREAM);
     sock.ssetsockopt(SOL_SOCKET, SO_REUSEADDR, 1);
     sock.sbind("", 53764);
